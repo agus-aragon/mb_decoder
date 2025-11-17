cdb0f4d344a91cd8d35004b8330a922b12f2fef4
A.AragonDaud <a.aragondaud@uliege.be>
2025-11-17 14:00:06 +0100

    psychopy to bids

--- src/00_bids/psychopy_to_bids.py
+++ src/00_bids/psychopy_to_bids.py
@@ -5,12 +5,12 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-datapath = Path("/Users/agusaragon/Documents/psychopy")
-subj = sys.argv[1]
-datapath_subj = datapath / f"subj-{subj}"
+datapath = Path("/data/project/mb_decoder/data/subj_raw/") #Path("/Users/agusaragon/Documents/psychopy")
+subj = '001' #sys.argv[1]
+datapath_subj = datapath / f"sub-{subj}" / "psychopy"
 # %%
-task = yaml.load(datapath_subj / "task-ES" / f"sub_{subj}_task-ES_exp.yaml")
-
+with open(datapath_subj / "task-ES" / f"sub_{subj}_task-ES_exp.yaml", 'r') as file:
+    task = pd.DataFrame(yaml.safe_load(file))
 
 
 
