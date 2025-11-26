9fd2d818150c493fc108d37ceae7af35458802bd
A.AragonDaud <a.aragondaud@uliege.be>
2025-11-26 14:53:11 +0100

    EEG to BIDS format + progress on events.tsv

--- src/00_bids/psychopy_to_bids.py
+++ src/00_bids/psychopy_to_bids.py
@@ -5,21 +5,22 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-datapath =Path("/Users/agusaragon/Documents/psychopy") #  Path("/data/project/mb_decoder/data/subj_raw/")
 subj = '001' #sys.argv[1]
-datapath_subj = datapath #/ f"sub-{subj}" #/ "psychopy"
+raw_dir = Path("/data/project/mb_decoder/data/subj_raw")#Path(sys.argv[2])
+bids_dir = Path("/data/project/mb_decoder/data/bids")#Path(sys.argv[3])
+datapath = raw_dir / f"sub-{subj}" / "psychopy"
 
 # Load all events file 
-with open(datapath_subj / "task-ES" / f"sub_{subj}_task-ES_ev.yaml", 'r') as file:
+with open(datapath / "task-ES" / f"sub_{subj}_task-ES_ev.yaml", 'r') as file:
     events = pd.DataFrame(yaml.safe_load(file))
 
 # Load task file 
-with open(datapath_subj / "task-ES" / f"sub_{subj}_task-ES_exp.yaml", 'r') as file:
+with open(datapath / "task-ES" / f"sub_{subj}_task-ES_exp.yaml", 'r') as file:
     task = pd.DataFrame(yaml.safe_load(file))
 task = task.set_index('trial_num')
 
 # %% Time baseline correction (sync with fMRI)
-first_trigger_onset = float( events[(events[1] == 'SCANNER') & (events[2] == 0)][0].iloc[0])
+first_trigger_onset = float(events[(events[1] == 'SCANNER') & (events[2] == 0)][0].iloc[0])
 
 task['probe_onset'] = task['probe_onset'] - first_trigger_onset
 task['prompt_onset'] = task['prompt_onset'] - first_trigger_onset
--- src/00_bids/src_to_bids.sh
+++ src/00_bids/src_to_bids.sh
@@ -1,26 +1,89 @@
-#!/bin/bash
-conda init
-conda activate mb_decoder
-SUBJECT="$1"
+########################################################
+## Start by organizing raw data like:                 ##  
+## /data/project/mb_decoder/data/subj_raw/sub-XXX/    ##
+## fMRI/ -> DICOMs + Physio                           ## 
+## eeg/ -> BrainVision files                          ##
+## psychopy/ -> behavioral data files                 ##
+########################################################
 
+#!/bin/bash
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate mb_decoder
+SUBJECT="$1"  # e.g., 001 (console usage: ./src_to_bids.sh 001)
+SUBJECT_ID_SCANNER = "$2" # e.g., 4472 (from fmriserver)
+
+BASE_DIR="/data/project/mb_decoder"
+RAW_DIR="${BASE_DIR}/data/subj_raw"
+BIDS_DIR="${BASE_DIR}/data/bids/mb_decoder"
+HOME_DIR="/home/agusaragon"
 echo "Converting sub-${SUBJECT} to BIDS format..."
 
-# Convert DICOMs to BIDS 
+######################## fMRI ########################
+# Convert DICOMs to BIDS using Heudiconv
+echo "Working on fMRI..."
 heudiconv \
-  -d /data/project/mb_decoder/data/subj_raw/sub-{subject}/fMRI/*_MR/*.dcm \
-  -o /data/project/mb_decoder/data/bids/mb_decoder/ \
-  -f /home/agusaragon/dev/mb_decoder/src/00_bids/heuristic.py \
+  -d "${RAW_DIR}/sub-{subject}/fMRI/*_MR/*.dcm" \
+  -o "${BIDS_DIR}" \
+  -f "${HOME_DIR}/dev/mb_decoder/src/00_bids/heuristic.py" \
   -s ${SUBJECT} \
   -c dcm2niix \
   -b \
   --overwrite
+echo "fMRI conversion complete."
 
-# Extract physio data
-dcm2bidsphysio --infile /data/project/mb_decoder/data/subj_raw/sub-${SUBJECT}/fMRI/Agustina_StdPE-70min_cmrr_mb2ep2d_1GE_TR1p5s_3p0mm_PhysioLog_16_MR/1.dcm \
-               --bidsprefix /data/project/mb_decoder/data/bids/mb_decoder/sub-${SUBJECT}/func/sub-${SUBJECT}_task-ES
+######################## Pshyio ########################
+#  Convert Physio DICOMs to BIDS using dcm2bidsphysio
+echo "Working on Physio..."
+dcm2bidsphysio --infile "${RAW_DIR}/sub-${SUBJECT}/fMRI/Agustina_StdPE-70min_cmrr_mb2ep2d_1GE_TR1p5s_3p0mm_PhysioLog_16_MR/1.dcm" \
+               --bidsprefix "${BIDS_DIR}/sub-${SUBJECT}/func/sub-${SUBJECT}_task-ES"
+dcm2bidsphysio --infile "${RAW_DIR}/sub-${SUBJECT}/fMRI/Agustina_StdPE-10min_cmrr_mb2ep2d_1GE_TR1p5s_3p0mm_PhysioLog_12_MR/1.dcm" \
+               --bidsprefix "${BIDS_DIR}/sub-${SUBJECT}/func/sub-${SUBJECT}_task-rest"
+echo "Physio conversion complete."
 
-dcm2bidsphysio --infile /data/project/mb_decoder/data/subj_raw/sub-${SUBJECT}/fMRI/Agustina_StdPE-10min_cmrr_mb2ep2d_1GE_TR1p5s_3p0mm_PhysioLog_12_MR/1.dcm \
-               --bidsprefix /data/project/mb_decoder/data/bids/mb_decoder/sub-${SUBJECT}/func/sub-${SUBJECT}_task-rest
+######################## EEG ########################
+echo "Working on EEG..."
 
-# datalad save -m "Converted subject ${SUBJECT} to BIDS format"
-echo "Conversion complete for subject ${SUBJECT}"
+# Modify Events in .vmrk files
+cd "${RAW_DIR}/sub-${SUBJECT}/eeg/"
+  # 1. Treat scanner toggle on/off as a single event "Scanner"
+sed -i 's/T  1_on/Scanner/g; s/T  1_off/Scanner/g' sub-${SUBJECT}_task-ES_eeg.vmrk
+sed -i 's/T  1_on/Scanner/g; s/T  1_off/Scanner/g' sub-${SUBJECT}_task-rest_eeg.vmrk
+  # 2. Task ES events
+sed -i 's/S 16/start_trial/g;
+  s/Stimulus,S  8/Response,response_arousal/g;
+  s/Stimulus,S  4/Response,response_probe/g;
+  s/S  2/probe/g;
+  s/Stimulus,S  1/EEG,start_eeg/g;
+  s/Stimulus,S 32/EEG,end_eeg/g
+' sub-${SUBJECT}_task-ES_eeg.vmrk
+
+# Convert EEG to BIDS using BVTools (Download Dotnet + clone repo first)
+cd "${HOME_DIR}/dev/BVTools/"
+export DOTNET_ROOT=$HOME/.dotnet
+export PATH=$PATH:$HOME/.dotnet
+dotnet run --project src/FileFormats/src/FileFormats.BrainVisionToBidsConverterCLI/FileFormats.BrainVisionToBidsConverterCLI.csproj -- \
+  --bv-header-file "${RAW_DIR}/sub-${SUBJECT}/eeg/sub-${SUBJECT}_task-ES_eeg.vhdr" \
+  --bids-destination-folder "${BIDS_DIR}" \
+  --subject ${SUBJECT} \
+  --task "ES"
+dotnet run --project src/FileFormats/src/FileFormats.BrainVisionToBidsConverterCLI/FileFormats.BrainVisionToBidsConverterCLI.csproj -- \
+  --bv-header-file "${RAW_DIR}/sub-${SUBJECT}/eeg/sub-${SUBJECT}_task-rest_eeg.vhdr" \
+  --bids-destination-folder "${BIDS_DIR}" \
+  --subject ${SUBJECT} \
+  --task "rest"
+echo "EEG conversion complete."
+
+######################### Psychopy events ########################
+python "${HOME_DIR}/dev/mb_decoder/src/00_bids/psychopy_to_bids.py" ${SUBJECT} ${RAW_DIR} ${BIDS_DIR}
+
+
+######################### Add to sourcedata ########################
+echo "s0${SUBJECT_ID_SCANNER}, sub-${SUBJECT}" >> "${BASE_DIR}/data/sourcedata/mapping.csv"
+
+
+######################### Save to datalad ########################
+# Save changes to datalad
+cd "${BIDS_DIR}"
+echo Saving changes to datalad...
+datalad save -m "Converted subject ${SUBJECT} to BIDS format"
+echo "Conversion to BIDS complete for sub-${SUBJECT}"
