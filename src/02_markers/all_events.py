5aae488268125d16fddf26530f6258011f45d86b
A.AragonDaud <a.aragondaud@uliege.be>
2026-07-28 17:07:17 +0200

    event extraction finalized

--- src/02_markers/all_events.py
+++ src/02_markers/all_events.py
@@ -15,7 +15,9 @@ TR = 1.5
 all_events = []
 all_events_df = pd.DataFrame()
 
+# Iterate through subjects
 for events_fname in data_path.glob(events_pattern):
+    # Load subject events.tsv
     print(events_fname)
     subject = events_fname.name.split("_")[0]
     df_events = pd.read_csv(events_fname, sep="\t")
@@ -26,30 +28,92 @@ for events_fname in data_path.glob(events_pattern):
         "timepoint": [],
         "event": [],
         "seconds_to_probe": [],
-        "response_probe": [],
+        "response_prompt": [],
         "rt_prompt": [],
         "response_arousal": [],
         "rt_arousal": [],
     }
 
+    # Extract all probe onset and start of trial
     probe_onsets_in_TR = (df_events["onset"] / TR).astype("int")
     first_timepoints_trial_TR = (
         (df_events["onset"] - df_events["rest_duration"]) / TR
     ).astype("int")
-    
+
+    # Iterate though each event
     for idx, row in df_events.iterrows():
-        this_probe_onset_in_TR = probe_onsets_in_TR[idx]
-        this_first_timepoints_trial_TR = first_timepoints_trial_TR[idx]
+        # Define probe onset
+        thisevent_probe_onset_in_TR = probe_onsets_in_TR[idx]
+
+        # Define start of trial
+        thisevent_first_timepoints_trial_TR = first_timepoints_trial_TR[idx]
+
+        # Define end of trial (= start of next trial)
+        if idx == df_events.index[-1]:
+            lastevent_extra_time_responses = (
+                df_events.loc[idx, "response_time_mental_state"]
+                + df_events.loc[idx, "response_time_arousal"]
+                + 5
+            )  # 5 seconds to account for lag between probe and prompt, and arousal prompt + rating (= 3 TRs)
+
+            thisevent_end_timepoint_trial_TR = (
+                df_events.loc[idx, "onset"] + lastevent_extra_time_responses
+            ) / TR
+        else:
+            thisevent_end_timepoint_trial_TR = first_timepoints_trial_TR[
+                idx + 1
+            ]
+
+        # Define timepoints before and after probe separatedly for
+        this_event_timepoints_TR = np.arange(
+            thisevent_first_timepoints_trial_TR,
+            thisevent_end_timepoint_trial_TR,
+            1,
+        )
+
+        total_timepoints = len(this_event_timepoints_TR)
+
+        # Seconds to probe
+        thisevent_seconds_to_probe = (
+            this_event_timepoints_TR - thisevent_probe_onset_in_TR
+        )
+        thisevent_n_timepoints_before_probe = (thisevent_seconds_to_probe < 0).sum()
+
+        thisevent_n_timepoints_after_probe = (thisevent_seconds_to_probe > 0).sum()
+
+        # Save this event information
+        this_events["subject"] += [subject] * total_timepoints
+
+        this_events["timepoint"] += this_event_timepoints_TR.tolist()
+
+        this_events["event"] += (
+            ["rest"] * thisevent_n_timepoints_before_probe
+            + ["probe"]
+            + ["response"] * thisevent_n_timepoints_after_probe
+        )
+        this_events["seconds_to_probe"] += thisevent_seconds_to_probe.tolist()
+
+        this_events["response_prompt"] += [
+            row["response_mental_state"]
+        ] * total_timepoints
+
+        this_events["rt_prompt"] += [
+            row["response_time_mental_state"]
+        ] * total_timepoints
+
+        this_events["response_arousal"] += [row["response_arousal"]] * total_timepoints
         
-        this_events["subject"] += (subject * XXX,)
-        this_events["timepoint"] += ([],)
-        this_events["event"] += ([],)
-        this_events["seconds_to_probe"] += ([],)
-        this_events["response_probe"] += (
-            [row["response_mental_state"]] * XXX,
-        )
-        this_events["rt_prompt"] += (
-            [row["response_time_mental_state"]] * XXX,
-        )
-        this_events["response_arousal"] += ([row["response_arousal"]] * XXX,)
-        this_events["rt_arousal"] += ([row["response_time_arousal"]] * XXX,)
+        this_events["rt_arousal"] += [
+            row["response_time_arousal"]
+        ] * total_timepoints
+
+    # Join subject data and all subject data
+    subject_events_df = pd.DataFrame(this_events)
+    all_events.append(subject_events_df)
+
+all_events_df = pd.concat(all_events)
+all_events_df.set_index(["subject", "timepoint"], inplace=True)
+print(all_events_df.shape)
+print(all_events_df.head())
+
+# %%
